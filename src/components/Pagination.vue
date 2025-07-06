2e8195dda11a83f251b40168bd9e3d9397fe38bd
lbjcena <lbjcena@gmail.com>
2025-07-07 01:10:15 +0200

    pagination mobile improvements

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,6 @@
 
 <script setup lang="ts">
+import { ChevronLeftSquareIcon, ChevronRightSquareIcon } from 'lucide-vue-next';
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 
@@ -7,6 +8,24 @@ const store = useStore()
 
 const totalPages = computed(() => store.getters.totalPages)
 const currentPage = computed(() => Number(store.state.currentPage))
+
+// ograniczenie ilości wyświetlanych przycisków paginacji - zapobiega overflow na mobilce
+
+const visiblePages = computed(() => {
+  const total = totalPages.value
+  const current = currentPage.value
+  // ile stron przed i po wyświetlić
+  const range = 1
+
+  const start = Math.max(1, current - range)
+  const end = Math.min(total, current + range)
+
+  const pages = []
+  for(let i = start; i <= end; i++){
+    pages.push(i)
+  }
+  return pages
+})
 
 function goToPage(page: number){
   store.commit('setPage', page)
@@ -29,12 +48,16 @@ function nextPage(){
 </script>
 <template>
   <div class="pagination">
-    <button class="pagination_controls" @click="previousPage">Poprzedni</button>
+    <button v-if="currentPage > 1" class="pagination_controls" @click="previousPage"><ChevronLeftSquareIcon width="35" height="35" stroke-width="1"/></button>
     <!-- przyciski z numerami stron -->
     <div class="pagination_pages">
-      <button v-for="page in totalPages" :key="page" :class="{active: page === currentPage}" @click="goToPage(page)">{{ page }}</button>
+      <button v-if="currentPage > 3" @click="goToPage(1)">{{ 1 }}</button>
+      <span v-if="currentPage > 3" class="text-center">...</span>
+      <button v-for="page in visiblePages" :key="page" :class="{active: page === currentPage}" @click="goToPage(page)">{{ page }}</button>
+      <span v-if="currentPage < totalPages - 1" class="text-center">...</span>
+      <button v-if="currentPage < totalPages - 1" @click="goToPage(totalPages)">{{totalPages}}</button>
     </div>
-    <button class="pagination_controls" @click="nextPage">Następny</button>
+    <button v-if="currentPage < totalPages"  class="pagination_controls" @click="nextPage"> <ChevronRightSquareIcon width="35" stroke-width="1" height="35"/> </button>
   </div>
 </template>
 
@@ -51,7 +74,7 @@ function nextPage(){
   @apply text-[16px];
 }
 .pagination_pages > * {
-  @apply border border-midnight p-1 min-h-[30px] min-w-[30px] text-[16px];
+  @apply border border-midnight p-1 min-h-[25px] min-w-[25px] text-[16px];
 }
 
 .pagination_controls {
